0390d2630a057d4e808251e8d14e98390e878920
RaviChowdary97 <aklvvsr1999@gmail.com>
2024-05-07 17:01:06 +0530

    changed

--- Front-end/src/pages/Dashboard.jsx
+++ Front-end/src/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Profile from "../components/Profile";
 import Posts from "../components/Posts";
 import DashComments from "../components/DashComments";
 import DashboardComp from "../components/DashboardComp";
+import { useSelector } from "react-redux";
 
 export default function Dashboard() {
+  const { currentUser } = useSelector((state) => state.user);
+
   const location = useLocation();
   const [tab, setTab] = useState("");
   useEffect(() => {
@@ -16,6 +19,9 @@ export default function Dashboard() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+  if (!currentUser) {
+    return <Navigate to="/sign-in" />;
+  }
   return (
     <>
       <div className="min-h-screen flex flex-col md:flex-row">
