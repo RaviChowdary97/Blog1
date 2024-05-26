abef706179b8fcf750fb5c4b979e5020fe291c92
RaviChowdary97 <aklvvsr1999@gmail.com>
2024-05-26 16:45:36 +0530

    code modified

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -55,7 +55,6 @@ export default function Header() {
           className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
           style={{ color: "#C9B0FD" }}
         >
-<<<<<<< HEAD
           <span
             className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white"
             style={{
@@ -63,39 +62,6 @@ export default function Header() {
               WebkitBackgroundClip: "text",
               WebkitTextFillColor: "transparent",
             }}
-=======
-          RealStorie's
-        </span>
-        Blog
-      </Link>
-      <form onSubmit={handleSubmit}>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-      <div className="flex gap-5 fw-bold">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/create-post">Create Post</Link>
-      </div>
-
-      <div>
-        {currentUser ? (
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar alt="user" img={currentUser.profilePicture} rounded />
-            }
->>>>>>> 562da6c45ea873cb84427a621388035ac1ee45f4
           >
             RealStorie's
           </span>
@@ -135,7 +101,7 @@ export default function Header() {
                   {currentUser.email}
                 </span>
               </Dropdown.Header>
-              <Link to="/dashboard?tab=profile">
+              <Link to={"/dashboard?tab=profile"}>
                 <Dropdown.Item>Profile</Dropdown.Item>
               </Link>
               <Dropdown.Divider />
